b518a89909cf791eaaf4648156120113005c7142
DmitriyPetrov-prog <dmitriy.petrov.1993@yandex.ru>
2021-01-04 22:02:06 +0300

    Init commit

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
+
+
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true,
+        port: 9000
+    },
+    plugins: [
+        new HtmlWebpackPlugin(),
+        new MiniCssExtractPlugin({
+            filename: 'bundle.css',
+        }),
+        new ESLintPlugin()
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'sass-loader'
+                ]
+
+            },
+
+        ]
+    }
+};
