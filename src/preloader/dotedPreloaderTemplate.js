e001090668908511c79c52a2a618868bcc33ab81
DmitriyPetrov-prog <dmitriy.petrov.1993@yandex.ru>
2021-01-12 23:30:51 +0300

    Refactor preloader styles to dotedPreloader names

--- src/preloader/dotedPreloaderTemplate.js
+++ src/preloader/dotedPreloaderTemplate.js
@@ -1,12 +1,12 @@
 const dotedPreloaderTemplate = () => {
     return  `
-        <div class="loader" data-type="preloader">
-            <span class="loader__text">Loading</span>
-            <div class="loader__container">
-                <div class="loader__dot loader__dot-1"></div>
-                <div class="loader__dot loader__dot-2"></div>
-                <div class="loader__dot loader__dot-3"></div>
-                <div class="loader__dot loader__dot-4"></div>
+        <div class="doted_loader" data-type="preloader">
+            <span class="doted_loader__text">Loading</span>
+            <div class="doted_loader__container">
+                <div class="doted_loader__dot doted_loader__dot-1"></div>
+                <div class="doted_loader__dot doted_loader__dot-2"></div>
+                <div class="doted_loader__dot doted_loader__dot-3"></div>
+                <div class="doted_loader__dot doted_loader__dot-4"></div>
             </div>
          </div>
     `;
