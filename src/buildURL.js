1558effde54d87abf3ae500ee66f215ec5f6a06d
DmitriyPetrov-prog <dmitriy.petrov.1993@yandex.ru>
2021-01-16 22:46:50 +0300

    Refactor Select and quizCategory(converted data for convenient operation ),add buildURL, add getDataFromSelect help function to getQuizOptions method from Menu, refactor menuTemplate,

---
+++ src/buildURL.js
@@ -0,0 +1,30 @@
+import {URL} from "./constants";
+
+const buildURL = (options) => {
+    const {questionQuantity, category, difficulty, type} = options;
+    return URL.BASE
+         + getApiAmount(questionQuantity)
+         + getApiCategory(category)
+         + getApiDifficulty(difficulty)
+         + getApiType(type);
+}
+
+const getApiAmount = (amount) => {
+    return amount ? `amount=${amount}` : "amount=10";
+}
+
+const getApiCategory = (category) => {
+    return category ? `&category=${category}` : "";
+}
+
+const getApiDifficulty = (difficulty) => {
+    return difficulty ? `&difficulty=${difficulty.toLowerCase()}` : "";
+}
+
+const getApiType = (type) => {
+    return type ? `&type=${type}` : "";
+}
+
+export {
+    buildURL
+}
